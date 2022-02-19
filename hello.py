34d117e6fd01beac4d57ca7a311fbce2e046df0a
Neil Burningham <neil.burningham@mediakind.com>
2022-02-19 14:15:46 +0000

    hello!

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("hello, fishface!")
+
+name = input("what is your name?! ")
+
+fish = 0
+fish = fish + 5
+
+print("well done", name, "you collected", fish, "fish!")
+
+print(f"well done {name} you collected {fish} fish!")
