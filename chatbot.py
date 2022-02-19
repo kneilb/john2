add464887ec0fa267836f7f21d3c96325e024f99
Neil Burningham <neil.burningham@mediakind.com>
2022-02-19 17:26:37 +0000

    chatbot updates!

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ print("Being a robot is tiring...")
 print("Night!")
 input("Are you gone yet?")
 print("GO AWAY I AM SLEEPING")
-print("Finally... I hate talking to humans... SNORE SNORE SNORE")
+input("Finally... I hate talking to humans... SNORE SNORE SNORE")
 print("Hi I am INTELLIGENT AND EVIL CHATBOT")
 digit = input("Give me a number...")
 digit2 = input(f"Give me another number {name}...")
@@ -18,6 +18,16 @@ time = input("What year is it?")
 
 abc = int(time) - 284
 print(f"oh yeh, I forgot, sorry.So if it is {time} and I am 284 I was born in {abc}!")
+age = input(f"How old are you?")
+ab = int(age) - int(time)
+print(f"WOH!!! You were born in {ab}! Thats like, nothing in robot!!!")
+print(f"Anyway you held me up for 99999999999999999 robot years so go away...")
+input("I SAID GO AWAY")
+input("Well, if you are going to stay then byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+print("*Vanishes*")
+
+
+
 
 
 
