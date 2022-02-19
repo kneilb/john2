68c7e2db62351f1c10aaf191bdf5e49467f25641
Neil Burningham <neil.burningham@mediakind.com>
2022-02-19 22:16:01 +0000

    Add crossroads, with death and goats

--- text-adventure.py
+++ text-adventure.py
@@ -51,19 +51,85 @@ class HaddockRoom(Room):
     def command(self, command):
         if ("fish" in command or "catch" in command) and "haddock" in command:
             print("You catch the haddock. It eats you all up!")
-            return "win"
+            print("Then it sneezes, and you are thrown into the air!")
+            print("After a long flight, you find yourself landing at a new location.")
+            return "crossroads"
 
         return None
+
+
+class CrossRoadsRoom(Room):
+    def __init__(self):
+        self.has_goat = True
+        self.goat_alive = True
+
+    def intro(self):
+        print("You are at a crossroads.")
+        print("There are exits to the north, south, east and west.")
+
+        if self.has_goat:
+            print()
+            if self.goat_alive:
+                print("There is a goat here.")
+            else:
+                print("There is an ex-goat here. It is not looking very well, and it smells a bit.")
+    
+    def command(self, command):
+        if self.has_goat and self.goat_alive and "goat" in command:
+            if "speak" in command or "talk" in command:
+                print("The goat seems interested, but struggles to communicate.")
+                print("It makes some happy bleating noises.")
+                return "crossroads"
+
+            if ("feed" in command or "give" in command) and ("cheese" in command or "haddock" in command):
+                print("The goat happily munches up the food you offer it.")
+                return "win"
+
+            if "shoot" in command:
+                print("You shoot at the goat, but miss and hit yourself in the knee.")
+                return "die"
+
+            if "kill" in command:
+                print("You manage to kill the goat with your bear hands (sic).")
+                self.goat_alive = False
+                return "crossroads"
+
+            if "take" in command or "carry" in command:
+                print("You pick up the goat")
+                self.has_goat = False
+                return "crossroads"
+
+        if "north" in command:
+            return "death"
+        
+        if "south" in command:
+            return "death"
+
+        if "east" in command:
+            return "death"
+
+        if "west" in command:
+            return "death"
+
+
+class DeathRoom(Room):
+    def intro(self):
+        print("You die! Please leave immediately.")
+        exit(0)
+
 
 class WinRoom(Room):
     def intro(self):
         print("You win! Please leave immediately.")
         exit(0)
 
+
 rooms = {
     "cheese": CheeseRoom(),
     "haddock": HaddockRoom(),
-    "win": WinRoom()
+    "crossroads": CrossRoadsRoom(),
+    "win": WinRoom(),
+    "death": DeathRoom()
 }
 
 current_room = rooms["cheese"]
