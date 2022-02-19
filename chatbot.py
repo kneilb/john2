af7db39f60f8493fa286982d1bd2c97438962439
Neil Burningham <neil.burningham@mediakind.com>
2022-02-19 16:47:45 +0000

    John chatbot wip

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+# Chatty chat bot
+print("I am a reallyyyyyyyyyyy chatty chat bot!")
+print("I love to talk!!!")
+name = input("What is your name?")
+job = input(f"Okay {name} what is your job???")
+print(f"WOW!!! I wish I did {job}!")
+print("Being a robot is tiring...")
+print("Night!")
+input("Are you gone yet?")
+print("GO AWAY I AM SLEEPING")
+print("Finally... I hate talking to humans... SNORE SNORE SNORE")
+print("Hi I am INTELLIGENT AND EVIL CHATBOT")
+digit = input("Give me a number...")
+digit2 = input(f"Give me another number {name}...")
+sum = int(digit) + int(digit2)
+print(f"Your numbers together are {sum}")
+time = input("What year is it?")
+
+abc = int(time) - 284
+print(f"oh yeh, I forgot, sorry.So if it is {time} and I am 284 I was born in {abc}!")
+
+
+
