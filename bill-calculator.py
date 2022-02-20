7bc3942ec5511f828db500493de61be759f48162
Neil Burningham <neil.burningham@mediakind.com>
2022-02-20 17:31:04 +0000

    Minor improvements to bill calculator

--- bill-calculator.py
+++ bill-calculator.py
@@ -1,11 +1,18 @@
 input("WELCOME TO BIll CALCULATOR press enter to continue")
+
 bill_amount = input("Please enter the bill amount:")
 tip_percentage = input("Please enter the tip percentage:")
 people_there = input("Please enter the amount of people sharing the bill:")
-tip_amount = int(bill_amount) * int(tip_percentage) / 100
-total = int(tip_amount) + int(bill_amount)
-tip_per = tip_amount / int(people_there)
-bill_per = int(total) / int(people_there)
+
+bill_amount = int(bill_amount)
+tip_percentage = int(tip_percentage)
+people_there = int(people_there)
+
+tip_amount = bill_amount * tip_percentage / 100
+total = tip_amount + bill_amount
+tip_per = tip_amount / people_there
+bill_per = total / people_there
+
 print(f"The tip amount is {tip_amount}.")
 print(f"The total is {total}.")
 print(f"The tip per person is {tip_per}.")
