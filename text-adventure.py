c6702ee0300890eac5f9e42770f53d2d6796aeef
Neil Burningham <neil.burningham@mediakind.com>
2022-02-20 11:29:23 +0000

    Add items & stuff

--- text-adventure.py
+++ text-adventure.py
@@ -1,4 +1,9 @@
-def get_command():
+import copy
+
+from typing import Optional
+
+
+def get_command() -> str:
     print("What is your command?")
     return input("# ")
 
@@ -8,10 +13,10 @@ def do_room(player, room):
 
     command = get_command()
 
-    if player.command(command):
+    if player.command(command, room):
         return
 
-    new_room = room.command(command)
+    new_room = room.command(command, player)
 
     if new_room:
         # print("I received room", new_room)
@@ -28,11 +33,17 @@ class Player(object):
     def __init__(self):
         self._items = {}
 
-    def command(self, command):
+    def remove_item(self, name):
+        return self._items.pop(name, None)
+
+    def add_item(self, item):
+        self._items[item.id] = item
+
+    def command(self, command: str, room) -> bool:
         if command in ["i", "inv", "inventory"]:
             if self._items:
                 print("You are carrying:")
-                for k, v in self._items:
+                for k, v in self._items.items():
                     print(f"{k} {v}")
             else:
                 print("Your pockets are empty!")
@@ -41,26 +52,54 @@ class Player(object):
             return True
 
         if "drop" in command:
-            item = command.split(" ")[-1]
-            if item not in self._items:
+            item_name = command.split(" ")[-1]
+
+            item = self.remove_item(item_name)
+            if not item:
                 print("Sorry, you don't seem to be carrying one of those!")
                 return True
+
+            print(f"You drop your {item_name}")
+
+            room.add_item(item)
 
         return False
 
 
 class Item(object):
-    pass
+    def __init__(self, id, description):
+        self.id = id
+        self._description = description
+
+    def description(self):
+        return self._description
+
+
+class Lantern(Item):
+    def __init__(self):
+        super().__init__("lantern", "A lantern.")
 
 
 class Goat(Item):
-    pass
+    def __init__(self):
+        super().__init__("goat", "A goat.")
+
+        self._alive = True
+        self._decomposition = 0
+
+    def description(self):
+        super().description()
+        if self._alive:
+            print("It seems to be breathing.")
+        else:
+            print("It appears to be somewhat dead.")
 
 
 class Room(object):
-    def __init__(self, intro="default room!", exits={}):
+    def __init__(self, intro="default room!", exits={}, items={}):
         self._intro = intro
         self._exits = exits
+        self._items = copy.deepcopy(items)
 
     def intro(self):
         print(self._intro)
@@ -75,37 +114,65 @@ class Room(object):
         else:
             print("There are no obvious exits.")
 
-    def command(self, command):
+        if self._items:
+            items = ", ".join(list(self._items.keys()))
+            print(f"There are some items here: {items}")
+
+    def command(self, command: str, player: Player) -> Optional[str]:
         if command in self._exits:
             return self._exits[command]
 
+        if "pick up" in command or "take" in command:
+            item_name = command.split(" ")[-1]
+            item = self.remove_item(item_name)
+            if not item:
+                print("Sorry, I can't find one of those!")
+            else:
+                print(f"You pick up the {item_name}.")
+
+            player.add_item(item)
+            # TODO: return non-none! (id of current room...)
+
         return None
+
+    def add_item(self, item):
+        self._items[item.id] = item
+
+    def remove_item(self, item_name: str) -> Optional[Item]:
+        return self._items.pop(item_name, None)
 
 
 class CheeseRoom(Room):
     def __init__(self):
-        super().__init__("You are in a room full of cheese.")
+        lantern = Lantern()
+        super().__init__(
+            "You are in a room full of cheese.",
+            {},
+            {
+                lantern.id: lantern
+            }
+        )
 
-    def command(self, command):
+    def command(self, command: str, player: Player) -> str:
         if "cheese" in command and "eat" in command:
             print("You eat the cheese, and fall down a massive hole to the next room")
             return "haddock"
 
-        return super().command(command)
+        return super().command(command, player)
 
 
 class HaddockRoom(Room):
     def __init__(self):
         super().__init__("You are in a room, featuring a pool containing a huge haddock.")
 
-    def command(self, command):
+    def command(self, command: str, player: Player) -> str:
         if ("fish" in command or "catch" in command) and "haddock" in command:
             print("You attempt to catch the haddock. It eats you all up!")
             print("Then it sneezes, and you are thrown into the air!")
             print("After a long flight, you find yourself landing at a new location.")
             return "crossroads"
 
-        return super().command(command)
+        return super().command(command, player)
 
 
 class CrossRoadsRoom(Room):
@@ -133,7 +200,7 @@ class CrossRoadsRoom(Room):
             else:
                 print("There is an ex-goat here. It is not looking very well, and it smells a bit.")
     
-    def command(self, command):
+    def command(self, command: str, player: Player):
         if self.has_goat and self.goat_alive and "goat" in command:
             if "speak" in command or "talk" in command:
                 print("The goat seems interested, but struggles to communicate.")
@@ -158,7 +225,7 @@ class CrossRoadsRoom(Room):
                 self.has_goat = False
                 return "crossroads"
 
-        return super().command(command)
+        return super().command(command, player)
 
 
 class DeadEndRoom(Room):
