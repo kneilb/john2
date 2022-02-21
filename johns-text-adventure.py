b9723eebdf8953b771be69aa0f03524d3f18b457
Neil Burningham <neil.burningham@mediakind.com>
2022-02-21 18:25:30 +0000

    john TA: use .lower()

--- johns-text-adventure.py
+++ johns-text-adventure.py
@@ -6,13 +6,13 @@ print("You are visiting a forest in Ireland with your friend.")
 print("You can take either:")
 item = input("Badger slippers, Fishsticks or a Tame beaver.") # item question
 print("You have arrived at the forest and are now walking to the lodge with your friend...")
-time.sleep(3) # 5 sec pause
+time.sleep(3) # 3 sec pause
 print(f'"Remember {name} we must not stray of the path!" He said.')
 time.sleep(3)
 print("A few seconds later and someone grabs you and pulls you of the path!")
 time.sleep(4)
 print('"If you give me fishsticks I will let you go!" He said...')
-if item == "Fishsticks" or item == "fishsticks" or item == "FISHSTICKS":
+if item.lower() == "fishsticks":                                                          # badger slippers
     time.sleep(3)
     print("You give him the fishsticks and he lets you go but your friend is nowhere to be seen...")
     time.sleep(3)
@@ -21,7 +21,7 @@ else:
     print("He traps you in a wooden cage wich is littered with skulls...")
     time.sleep(6)
     print("You notice a badger run past the cage and into a bush")
-    if item == "Badger slippers" or item == "Badger Slippers" or item == "badger slippers":
+    if item.lower() == "badger slippers":                                         # badger
         print("The badger reappears and starts speaking to your badger slippers!")
         print('"Hey! I am going to break you out of here!"')
         print("The badger picks the lock using its claws and then the door swings open.")
@@ -29,11 +29,11 @@ else:
         print("You walk around for a while before finding camp!")
         time.sleep(3)
         print("WELL DONE THANKS FOR PLAYING!!!")
-    else:
-        if item == "Tame beaver" or item == "Tame Beaver" or item == "tame beaver":
-            print("Your beaver chews open the door hinges and it swings open.")
-            time.sleep(5)
-            print("You walk around a bit then realise that you are still lost!")
+    
+    elif item.lower() == "tame beaver":                                      # tame beaver
+        print("Your beaver chews open the door hinges and it swings open.")
+        time.sleep(5)
+        print("You walk around a bit then realise that you are still lost!")
         
         
         
