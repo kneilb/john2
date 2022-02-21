0a056737d71deccacb18fb53f9d8c1a02477fc76
Neil Burningham <neil.burningham@mediakind.com>
2022-02-21 13:13:09 +0000

    John's text adventure

---
+++ johns-text-adventure.py
@@ -0,0 +1,44 @@
+import time
+input("WELCOME TO THE LOST FOREST press enter to continue")
+name = input("Please enter your name:")
+# game starts
+print("You are visiting a forest in Ireland with your friend.")
+print("You can take either:")
+item = input("Badger slippers, Fishsticks or a Tame beaver.") # item question
+print("You have arrived at the forest and are now walking to the lodge with your friend...")
+time.sleep(3) # 5 sec pause
+print(f'"Remember {name} we must not stray of the path!" He said.')
+time.sleep(3)
+print("A few seconds later and someone grabs you and pulls you of the path!")
+time.sleep(4)
+print('"If you give me fishsticks I will let you go!" He said...')
+if item == "Fishsticks" or item == "fishsticks" or item == "FISHSTICKS":
+    time.sleep(3)
+    print("You give him the fishsticks and he lets you go but your friend is nowhere to be seen...")
+    time.sleep(3)
+    print("You are now lost in the forest.")
+else:
+    print("He traps you in a wooden cage wich is littered with skulls...")
+    time.sleep(6)
+    print("You notice a badger run past the cage and into a bush")
+    if item == "Badger slippers" or item == "Badger Slippers" or item == "badger slippers":
+        print("The badger reappears and starts speaking to your badger slippers!")
+        print('"Hey! I am going to break you out of here!"')
+        print("The badger picks the lock using its claws and then the door swings open.")
+        time.sleep(10)
+        print("You walk around for a while before finding camp!")
+        time.sleep(3)
+        print("WELL DONE THANKS FOR PLAYING!!!")
+    else:
+        if item == "Tame beaver" or item == "Tame Beaver" or item == "tame beaver":
+            print("Your beaver chews open the door hinges and it swings open.")
+            time.sleep(5)
+            print("You walk around a bit then realise that you are still lost!")
+        
+        
+        
+
+
+
+
+
