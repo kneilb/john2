895d9f8636be9838627711fbaca0a63c246fa1e4
Neil Burningham <neil.burningham@mediakind.com>
2022-02-21 18:25:03 +0000

    Update password machine to random!

--- password-machine.py
+++ password-machine.py
@@ -1,10 +1,15 @@
+import random
+password = random.randint(1, 10000)
+
 input("WELCOME TO PASSWORD MACHINE press enter to continue")
+
 while True:
-    password = input("Enter the password:")
-    if password == "Daddyisbest" or password == "6547":
+    guess = input("Enter the password:")
+    guess = int(guess)
+    if guess == password:
         print("System online")
         break
-    elif int(password) < 6547:
+    elif guess < password:
         print("Your password is too low!")
     else:
         print("Your password is too high!")
