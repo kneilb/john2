6d23145998b7972d5af0ebfc3eb91cb579be20cd
Neil Burningham <neil.burningham@mediakind.com>
2022-03-05 16:42:43 +0000

    Dice game development!

--- dice-game.py
+++ dice-game.py
@@ -1,12 +1,25 @@
 import random
 import time
 
-# function
+# functions
 def dice_roll(times):
     dice = []
     for a in range(times):
         dice.append(random.randint(1, 6))
     return dice
+
+def winner():
+    if total > total2:
+        print(f"{name} wins!")
+    else:
+        print(f"{name2} wins!")
+
+def roll_again(choices, list):
+    print("rolling again...")
+    time.sleep(random.randint(1, 3))
+    for a in range (len(choices)):
+        if choices[a] == "-":
+            list[a] = random.randint(1, 6)
 
 # main
 input("WELCOME TO THROW OFF!!! Press enter to continue!")
@@ -25,8 +38,17 @@ print(f"{dice_list}")
 total = sum(dice_list)
 print(f"{total}")
 
+choice = input("Press _ to keep or - to throw again: ")
+while len(choice) != times:
+    print(f"You must enter {times} characters.")
+    choice = input("Press _ to keep or - to throw again: ")
+roll_again(choice, dice_list)
+print(f"{dice_list}")
+total = sum(dice_list)
+print(f"{total}")
+
 # computer code
-name_list = ["Holly", "Andrew", "Michael", "Felix", "Raphiel", "Ellis", "Bella", "Charlie"]
+name_list = ["Holly", "Andrew", "Michael", "Felix", "Archie", "Ellis", "Bella", "Charlie"]
 name2 = random.choice(name_list)
 
 # computer go
@@ -36,3 +58,5 @@ print(f"{name2}'s roll.")
 print(f"{dice_list2}")
 total2 = sum(dice_list2)
 print(f"{total2}")
+
+winner()
