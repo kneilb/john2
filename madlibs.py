132ab10b834ab064de99f0b8219316dccd586d0e
Neil Burningham <neil.burningham@mediakind.com>
2022-03-13 16:24:48 +0000

    Madlibs updates

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,8 @@
 import random
-input("WELCOME TO MAD LIBS press enter to play")
-animal = input("Give me the name of a animal...")
-colour = input("Give me the name of a colour...")
-description = input("Give me an adjective for an object...")
+input("WELCOME TO MAD LIBS press enter to play!")
+animal = input("Give me the name of an animal... ")
+colour = input("Give me a colour... ")
+description = input("Give me an adjective for an object... ")
 print()
 sentence = [
     f"The {colour} {animal} disappeared into the {description} hedge!",
@@ -14,7 +14,9 @@ sentence = [
     f"The {description} {animal} was shot by a man wearing a {colour} coat!",
     f"The strange {animal} flew into the {colour} sky as a {description} rock fell on its head and killed it!",
     f"The {colour} {animal} teleported onto the {description} tree!",
-    f"The man threw a {animal} onto a {description} ledge above a pit of {colour} deer!"
+    f"The man threw a {animal} onto a {description} ledge above a pit of {colour} deer!",
+    f"The {colour} man threw the {description} rock at the {animal}!",
+    f"The {description} boulder crushed the {animal} who was sitting on a {colour} tree!"
 ]
 print(random.choice(sentence))
 # print(sentence[1])
