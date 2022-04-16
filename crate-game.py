390de1a9ec512bedf8a964a72fb8f0d133f23c5b
Neil Burningham <neil.burningham@mediakind.com>
2022-04-16 13:39:02 +0100

    More crates!

--- crate-game.py
+++ crate-game.py
@@ -12,7 +12,10 @@ CRATE_SIZE = 30
 canvas = Canvas(window, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 canvas.pack()
 
-crate_type_list = ["red", "brown"]
+crate_type_list = ["red", "black"]
+
+black_crates = []
+red_crates = []
 
 # functions
 def orb_move(event):
@@ -28,8 +31,28 @@ def drop_crate():
     y_drop = 0
     crate_type = random.choice(crate_type_list)
     crate = canvas.create_rectangle(x_drop, y_drop, x_drop + CRATE_SIZE, y_drop + CRATE_SIZE, fill=crate_type)
+    if crate_type == "red":
+        red_crates.append(crate)
+    else:
+        black_crates.append(crate)
+    window.after(1000, drop_crate)
 
-# main
+def move_crate():
+    all_crates = black_crates + red_crates
+    for c in all_crates:
+        canvas.move(c, 0, crate_speed)
+        if canvas.coords(c)[1] > CANVAS_HEIGHT:
+            new_x = random.randint(1, 470)
+            canvas.coords(c, new_x, 0, new_x + CRATE_SIZE, CRATE_SIZE)
+    window.after(50, move_crate)
+
+def collision(player, crate):
+    x_close = abs(canvas.coords(player)[0] - canvas.coords(crate)[0])
+    y_close = abs(canvas.coords(crate)[0] - canvas.coords(player)[0])
+
+##########
+## MAIN ##
+##########
 main_message = canvas.create_text(200, 200, text = "crate collecter!!!")
 crates_got = 0
 message1 = canvas.create_text(200, 300, text = f"you collected {crates_got} crates!")
@@ -37,9 +60,8 @@ message1 = canvas.create_text(200, 300, text = f"you collected {crates_got} crat
 orb = canvas.create_oval(CANVAS_WIDTH/2 - PLAYER_SIZE/2, CANVAS_HEIGHT - PLAYER_SIZE, CANVAS_WIDTH/2 + PLAYER_SIZE/2, CANVAS_HEIGHT, fill="blue")
 canvas.bind_all("<Key>", orb_move)
 
-brown_crate = 0
-red_crate = 0
-crate_supply_speed = 2
-drop_crate()
-window.after(50, drop_crate)
+crate_speed = 2
+
+window.after(5000, drop_crate)
+window.after(1000, move_crate)
 window.mainloop()
