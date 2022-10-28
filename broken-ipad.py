0bde248674dac248f58866492aaab8894f6f3b75
Neil Burningham <neil.burningham@mediakind.com>
2022-10-28 10:08:02 +0100

    google->gogle, add fish catcher

--- broken-ipad.py
+++ broken-ipad.py
@@ -3,13 +3,13 @@ import time
 password = random.randint(1, 100) #100
 
 # google #
-def google():
+def gogle():
     print()
-    google_search = input("What shall you search? ")
-    print(f"Sorry, {google_search} is not valid.")
-    exit_google = input("would you like to exit google? y for yes and n for no: ")
-    if exit_google == "n":
-        google()
+    gogle_search = input("What shall you search? ")
+    print(f"Sorry, {gogle_search} is not valid.")
+    exit_gogle = input("would you like to exit gogle? y for yes and n for no: ")
+    if exit_gogle == "n":
+        gogle()
     else:
         computer_is_on()
 
@@ -35,17 +35,36 @@ def gmail():
         else:
             computer_is_on()
 
+def fish_catcher():
+    fish_caught = ("You caught a fish!", "You caught a fish!", "You waited, but no fish came.")
+    one = random.choice(fish_caught)
+    two = random.choice(fish_caught)
+    three = random.choice(fish_caught)
+    time.sleep(3)
+    print("where would you like to fish?")
+    fishing_spot = input("1, 2 or 3? ")
+    if fishing_spot == "1":
+        print(one)
+    elif fishing_spot == "2":
+        print(two)
+    elif fishing_spot == "3":
+        print(three)
+    else:
+        print("what you SAY!??!")
+
 ##################
 # COMPUTER IS ON #
 ##################
 def computer_is_on():
     time.sleep(2.5)
     print()
-    app_chosen = input("Would you like to use: google, gmail, fish catcher, cheese monkey simulator or the app store? ")
-    if app_chosen == "google":
-        google()
-    if app_chosen == "gmail":
+    app_chosen = input("Would you like to use: gogle, gmail, fish catcher, cheese monkey simulator or the app store? ")
+    if app_chosen == "gogle":
+        gogle()
+    elif app_chosen == "gmail":
         gmail()
+    elif app_chosen == "fish catcher":
+        fish_catcher()
 
 # start #
 def start():
@@ -53,6 +72,7 @@ def start():
     guess = int(guess)
     if guess == password:
         print("System online")
+        time.sleep(1)
         computer_is_on()
     if guess < password:
         print("Your password is too low!")
@@ -61,4 +81,5 @@ def start():
         print("Your password is too high!")
         start()
 
-start()
+fish_catcher()
+# start()
